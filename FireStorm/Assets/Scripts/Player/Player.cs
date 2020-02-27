a95d7d51aab804edb64953165a6443d2e16e46ed
peraltazera <49589848+peraltazera@users.noreply.github.com>
2020-02-27 18:04:12 -0300

    Master1

---
+++ FireStorm/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rigid;
+    public float speed;
+    
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float movX = Input.GetAxis("Horizontal");
+        float movZ = Input.GetAxis("Vertical");
+        rigid.velocity = new Vector3(movX * speed, rigid.velocity.y, movZ * speed);
+    }
+}
